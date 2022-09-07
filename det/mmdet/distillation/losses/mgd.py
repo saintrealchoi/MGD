efededfe0d83ae92aff9e0fcc76cbd1275f91e12
choisj <csj7480@gmail.com>
2022-09-07 15:54:12 +0900

    Random Mask to parts of layer

--- det/mmdet/distillation/losses/mgd.py
+++ det/mmdet/distillation/losses/mgd.py
@@ -42,7 +42,8 @@ class FeatureLoss(nn.Module):
                 preds_S,
                 preds_T,
                 gt_bboxes,
-                img_metas):
+                img_metas,
+                loss_name):
         """Forward function.
         Args:
             preds_S(Tensor): Bs*C*H*W, student's feature map
@@ -50,44 +51,35 @@ class FeatureLoss(nn.Module):
             gt_bboxes(tuple): Bs*[nt*4], pixel decimal: (tl_x, tl_y, br_x, br_y)
             img_metas (list[dict]): Meta information of each image, e.g.,
             image size, scaling factor, etc.
+            loss_name(string): Feature Loss name for partial layer distillation
         """
         assert preds_S.shape[-2:] == preds_T.shape[-2:]
 
         if self.align is not None:
             preds_S = self.align(preds_S)
     
-        loss = self.get_dis_loss(preds_S, preds_T, gt_bboxes, img_metas)*self.alpha_mgd
+        loss = self.get_dis_loss(preds_S, preds_T, gt_bboxes, img_metas, loss_name)*self.alpha_mgd
             
         return loss
 
-    def get_dis_loss(self, preds_S, preds_T, gt_bboxes, img_metas):
+    def get_dis_loss(self, preds_S, preds_T, gt_bboxes, img_metas, loss_name):
         loss_mse = nn.MSELoss(reduction='sum')
         N, _, H, W = preds_T.shape
 
         device = preds_S.device
 
-        Mask = torch.ones(N,H,W).to(device)
-        for i in range(N):
-            # new_boxxes = torch.ones_like(gt_bboxes[i]).to(device)
-            new_boxxes = torch.ones_like(gt_bboxes[i]).to(device)
-            new_boxxes[:, 0] = gt_bboxes[i][:, 0]/img_metas[i]['img_shape'][1]*W
-            new_boxxes[:, 2] = gt_bboxes[i][:, 2]/img_metas[i]['img_shape'][1]*W
-            new_boxxes[:, 1] = gt_bboxes[i][:, 1]/img_metas[i]['img_shape'][0]*H
-            new_boxxes[:, 3] = gt_bboxes[i][:, 3]/img_metas[i]['img_shape'][0]*H
+        if loss_name[-1]=='3' or loss_name[-1]=='4':
+            new_fea = self.generation(preds_S)
+            
+            dis_loss = loss_mse(new_fea, preds_T)/N
+            return dis_loss
 
-            new_boxxes = new_boxxes.int()
+        else:
+            mat = torch.rand((N,1,H,W)).to(device)
+            mat = torch.where(mat>1-self.lambda_mgd, 0, 1).to(device)
 
-            for k in range(len(new_boxxes)):
-                Mask[i][new_boxxes[k][1]:new_boxxes[k][3]][new_boxxes[k][0]:new_boxxes[k][2]]=0
+            masked_fea = torch.mul(preds_S, mat)
+            new_fea = self.generation(masked_fea)
 
-        Mask = Mask.unsqueeze(dim=1)
-        mat_rand = torch.rand((N,1,H,W)).to(device)
-        mat_rand = torch.where(mat_rand>1-self.lambda_mgd, 0, 1).to(device)
-        mat_final = torch.logical_or(Mask,mat_rand).int()
-
-        masked_fea = torch.mul(preds_S, mat_final)
-        new_fea = self.generation(masked_fea)
-
-        dis_loss = loss_mse(new_fea, preds_T)/N
-
-        return dis_loss
+            dis_loss = loss_mse(new_fea, preds_T)/N
+            return dis_loss
