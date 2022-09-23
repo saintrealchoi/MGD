4ff7ad58f211913d80597901fbe1da4d6ac03e9e
choisj <csj7480@gmail.com>
2022-09-23 15:06:50 +0900

    Add Config files

---
+++ det/configs/distillers/mgd/faster_rcnn_rx101_32x4d_distill_faster_rcnn_r50_fpn_2x_coco_bg.py
@@ -0,0 +1,144 @@
+_base_ = [
+    '../../faster_rcnn/faster_rcnn_r50_fpn_2x_coco.py'
+]
+# model settings
+find_unused_parameters=True
+alpha_mgd=0.0000005
+lambda_mgd=0.45
+beta_mgd=0.0000002
+distiller = dict(
+    type='DetectionDistiller',
+    teacher_pretrained = 'https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_x101_64x4d_fpn_1x_coco/faster_rcnn_x101_64x4d_fpn_1x_coco_20200204-833ee192.pth',
+    distill_cfg = [ dict(student_module = 'neck.fpn_convs.3.conv',
+                         teacher_module = 'neck.fpn_convs.3.conv',
+                         output_hook = True,
+                         methods=[dict(type='FeatureLoss',
+                                       name='loss_mgd_fpn_3',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_mgd=alpha_mgd,
+                                       beta_mgd=beta_mgd,
+                                       lambda_mgd=lambda_mgd,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.2.conv',
+                         teacher_module = 'neck.fpn_convs.2.conv',
+                         output_hook = True,
+                         methods=[dict(type='FeatureLoss',
+                                       name='loss_mgd_fpn_2',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_mgd=alpha_mgd,
+                                       beta_mgd=beta_mgd,
+                                       lambda_mgd=lambda_mgd,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.1.conv',
+                         teacher_module = 'neck.fpn_convs.1.conv',
+                         output_hook = True,
+                         methods=[dict(type='FeatureLoss',
+                                       name='loss_mgd_fpn_1',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_mgd=alpha_mgd,
+                                       beta_mgd=beta_mgd,
+                                       lambda_mgd=lambda_mgd,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.0.conv',
+                         teacher_module = 'neck.fpn_convs.0.conv',
+                         output_hook = True,
+                         methods=[dict(type='FeatureLoss',
+                                       name='loss_mgd_fpn_0',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_mgd=alpha_mgd,
+                                       beta_mgd=beta_mgd,
+                                       lambda_mgd=lambda_mgd,
+                                       )
+                                ]
+                        ),
+                   ],
+    bg_distill_cfg = [ dict(student_module = 'neck.fpn_convs.3.conv',
+                         teacher_module = 'neck.fpn_convs.3.conv',
+                         output_hook = True,
+                         methods=[dict(type='FeatureLoss',
+                                       name='loss_mgd_bg_fpn_3',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_mgd=alpha_mgd,
+                                       beta_mgd=beta_mgd,
+                                       lambda_mgd=lambda_mgd,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.2.conv',
+                         teacher_module = 'neck.fpn_convs.2.conv',
+                         output_hook = True,
+                         methods=[dict(type='FeatureLoss',
+                                       name='loss_mgd_bg_fpn_2',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_mgd=alpha_mgd,
+                                       beta_mgd=beta_mgd,
+                                       lambda_mgd=lambda_mgd,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.1.conv',
+                         teacher_module = 'neck.fpn_convs.1.conv',
+                         output_hook = True,
+                         methods=[dict(type='FeatureLoss',
+                                       name='loss_mgd_bg_fpn_1',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_mgd=alpha_mgd,
+                                       beta_mgd=beta_mgd,
+                                       lambda_mgd=lambda_mgd,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.0.conv',
+                         teacher_module = 'neck.fpn_convs.0.conv',
+                         output_hook = True,
+                         methods=[dict(type='FeatureLoss',
+                                       name='loss_mgd_bg_fpn_0',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_mgd=alpha_mgd,
+                                       beta_mgd=beta_mgd,
+                                       lambda_mgd=lambda_mgd,
+                                       )
+                                ]
+                        ),
+                   ]
+    )
+
+teacher_cfg = 'configs/faster_rcnn/faster_rcnn_x101_64x4d_fpn_1x_coco.py'
+student_cfg = 'configs/faster_rcnn/faster_rcnn_r50_fpn_2x_coco.py'
+# optimizer_config = dict(_delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
+
+data = dict(
+    samples_per_gpu=8,
+    workers_per_gpu=2)
+
+log_config = dict(
+    interval=50,
+    hooks=[
+        dict(type="TextLoggerHook", by_epoch=False),
+        dict(
+            type="WandbLoggerHook",
+            init_kwargs=dict(
+                project="KD",
+                name="FRCNN(MGD)-w/ RM,w/ bg",
+                config=dict(
+                    work_dirs="work_dirs/bg",
+                ),
+            ),
+            # by_epoch=False,
+        ),
+    ],
+)
